4099afa9dfb3c13f412ce3badea375db46a751f2
ganbud_munkhtulga88 <76542111+Kclaus88@users.noreply.github.com>
2021-05-03 00:47:25 +0800

    Delete navbar.js

--- navbar.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelector('.drop-trigger');
-    let instances = M.Dropdown.init(elems, {
-        belowOrigin: true
-    })
-});
